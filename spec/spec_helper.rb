74250310e7c92816e46328f4ad0062a7812afa93
Papito <eu@papito.io>
2018-08-21 10:15:22 -0300

    Update spec_helper.rb
    tfs testes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,24 +27,24 @@ RSpec.configure do |config|
   end
 end
 
-Capybara.javascript_driver = :selenium
-Capybara.run_server = false
+# Capybara.javascript_driver = :selenium
+# Capybara.run_server = false
 
-args = ['--no-default-browser-check']
-caps = Selenium::WebDriver::Remote::Capabilities.chrome(
-  'chromeOptions' => { 'args' => args }
-)
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :remote,
-    url: 'http://selenium_server:4444/wd/hub',
-    desired_capabilities: caps
-  )
-end
+# args = ['--no-default-browser-check']
+# caps = Selenium::WebDriver::Remote::Capabilities.chrome(
+#   'chromeOptions' => { 'args' => args }
+# )
+# Capybara.register_driver :selenium do |app|
+#   Capybara::Selenium::Driver.new(
+#     app,
+#     browser: :remote,
+#     url: 'http://selenium_server:4444/wd/hub',
+#     desired_capabilities: caps
+#   )
+# end
 
 Capybara.configure do |config|
-  config.default_driver = :selenium
+  config.default_driver = :selenium_chrome_headless
   config.app_host = 'https://www.dotz.com.br'
 end
 
