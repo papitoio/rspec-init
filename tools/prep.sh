656f13df2c52285e8d0eb77d2d8a79b576ca47ea
Fernando Papini <papini@dotz.com>
2018-08-23 16:33:37 -0300

    vai planeta

--- tools/prep.sh
+++ tools/prep.sh
@@ -1,5 +1,5 @@
 apt-get update; apt-get install unzip default-jre -y 
 wget https://bintray.com/qameta/generic/download_file?file_path=io%2Fqameta%2Fallure%2Fallure%2F2.7.0%2Fallure-2.7.0.zip
-if [ -d allure-2.7.0 ]; then rmdir allure-2.7.0; fi
+if [ -d allure-2.7.0 ]; then rm -rf allure-2.7.0; fi
 unzip *.zip
 rm *.zip
